55dc99218433782874c61330cf8452cc809fb7de
example-collections <example-collections@gmail.com>
2023-06-03 17:26:47 +0900

    useNavigate example

--- react-router-v6/src/components/Header.tsx
+++ react-router-v6/src/components/Header.tsx
@@ -1,6 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
+/**
+ * useNavigate
+ * Link의 용도와 마찬가지로 페이지 이동을 하게 하는 함수
+ */
 function Header() {
+  const navigate = useNavigate();
+  const onAboutClick = () => {
+    navigate("/about");
+  };
   return (
     <header>
       <ul>
@@ -8,7 +16,7 @@ function Header() {
           <Link to={"/"}>Home</Link>
         </li>
         <li>
-          <Link to={"/about"}>About</Link>
+          <button onClick={onAboutClick}>About</button>
         </li>
       </ul>
     </header>
