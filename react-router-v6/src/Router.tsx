51311c56d16f428cb9b02aee4935cddac81758e7
example-collections <example-collections@gmail.com>
2023-06-03 17:21:02 +0900

    errorElement example

--- react-router-v6/src/Router.tsx
+++ react-router-v6/src/Router.tsx
@@ -2,7 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./screens/Home";
 import About from "./screens/About";
 import Root from "./Root";
+import NotFound from "./screens/NotFound";
+import ErrorComponent from "./components/ErrorComponent";
 
+/**
+ * errorElement
+ * 에러가 생긴 경우 보여주는 페이지
+ * 다른 페이지가 에러의 영향을 받지 않게 해준다.
+ */
 const router = createBrowserRouter([
   {
     path: "/",
@@ -11,12 +18,14 @@ const router = createBrowserRouter([
       {
         path: "",
         element: <Home />,
+        errorElement: <ErrorComponent />,
       },
       {
         path: "about",
         element: <About />,
       },
     ],
+    errorElement: <NotFound />,
   },
 ]);
 
